24ef803cb24c932237bbaf39734719a398c618d3
Carlo Liwanag <ecaliwanag@gmail.com>
2019-05-23 00:07:11 +0800

    added a open  low high line graph in stock detail

--- src/@shared/ui/stock-detail/stock-detail.component.ts
+++ src/@shared/ui/stock-detail/stock-detail.component.ts
@@ -1,9 +1,12 @@
-import { Input, Component, ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
+import { 
+  Input, 
+  Component, 
+  ChangeDetectionStrategy, 
+  OnChanges, 
+  SimpleChanges 
+} from '@angular/core';
 
-@Component({
-  template: `
-    <div></div>
-  `,
+@Component({  
   selector: 'app-stock-detail',
   templateUrl: './stock-detail.component.html',
   styleUrls: ['./stock-detail.component.scss'],
@@ -12,11 +15,81 @@ import { Input, Component, ChangeDetectionStrategy, OnChanges, SimpleChanges } f
 export class StockDetailComponent implements OnChanges {
   @Input() chartData;
 
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Date';
+  showYAxisLabel = true;
+  yAxisLabel = 'Open Low High';
+  timeline = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  openLowHigh: any[] = [];
+
+  
+
   ngOnChanges(changes: SimpleChanges) {
-    if (changes.chartData) {
-      // do something here
+    if (changes.chartData && this.chartData) {
+      // do something       
+      this.createOpenLowHighData();
     }
   }
 
+  createOpenLowHighData() {
+    if (this.chartData) {
+      let open = this.chartData.historical_details.map(historicalDetail => {
+        return {
+          name: historicalDetail.fetchdate,
+          value: historicalDetail.Open,          
+          max: historicalDetail.Open + 500
+        };
+      });
 
+      let low = this.chartData.historical_details.map(historicalDetail => {
+        return {
+          name: historicalDetail.fetchdate,
+          value: historicalDetail.Low,          
+          max: historicalDetail.Low + 500
+        };
+      });
+
+      let high = this.chartData.historical_details.map(historicalDetail => {
+        return {
+          name: historicalDetail.fetchdate,
+          value: historicalDetail.High,          
+          max: historicalDetail.High + 500
+        };
+      });
+
+      this.openLowHigh = [
+        {
+          name: 'Open',
+          series: this.sortByKey(open, 'name')
+        },
+        {
+          name: 'Low',
+          series: this.sortByKey(low, 'name')
+        },
+        {
+          name: 'High',
+          series: this.sortByKey(high, 'name')
+        }
+      ];      
+    }
+  }
+
+  sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+  }
 }
