623ae0adae5466e5112504aba20b74ad6ec0e117
Nyl Brian Ong <nylbrian@gmail.com>
2019-05-26 00:59:45 +0800

    Fix styling for News component

--- src/@shared/ui/stock-news/stock-news.component.ts
+++ src/@shared/ui/stock-news/stock-news.component.ts
@@ -1,27 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { StocksService } from '@shared/services';
+import { Component, OnInit } from "@angular/core";
+import { StocksService } from "@shared/services";
 
 @Component({
-  selector: 'app-stock-news',
-  templateUrl: './stock-news.component.html',
-  styleUrls: ['./stock-news.component.scss']
+  template: `
+    <mat-list class="Stock-news">
+      <mat-list-item *ngFor="let news of stockNews; last as last">
+        <p>{{ news }}</p>
+        <mat-divider [inset]="true" *ngIf="!last"></mat-divider>
+      </mat-list-item>
+    </mat-list>
+  `,
+  selector: "app-stock-news",
+  styleUrls: ["./stock-news.component.scss"]
 })
 export class StockNewsComponent implements OnInit {
-
-  stockCode:string = 'GOOG';
+  stockCode: string = "GOOG";
 
   stockNews: any[] = [];
 
-  constructor(private stockService: StocksService) { }
+  constructor(private stockService: StocksService) {}
 
   ngOnInit() {
-    if (this.stockCode) {      
-      this.stockService.getNewsByStockSymbol(this.stockCode)
+    if (this.stockCode) {
+      this.stockService
+        .getNewsByStockSymbol(this.stockCode)
         .toPromise()
-        .then((data) => {
+        .then(data => {
           this.stockNews = data.TOP_NEWS;
         });
-    }    
+    }
   }
-
 }
