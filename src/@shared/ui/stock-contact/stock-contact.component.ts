f8fdeb43b958371a3c8cd73914df508487d52300
Nyl Brian Ong <nylbrian@gmail.com>
2019-08-11 01:56:44 +0800

    Re-order Contact fields and format links

--- src/@shared/ui/stock-contact/stock-contact.component.ts
+++ src/@shared/ui/stock-contact/stock-contact.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, ViewChild } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild
+} from "@angular/core";
 import { MatSort, MatTableDataSource } from "@angular/material";
 import { Contact } from "@shared/services";
 
@@ -24,7 +32,24 @@ import { Contact } from "@shared/services";
       <!-- Value Column -->
       <ng-container matColumnDef="value">
         <th mat-header-cell *matHeaderCellDef mat-sort-header>Value</th>
-        <td mat-cell *matCellDef="let element">{{ element.value }}</td>
+        <td mat-cell *matCellDef="let element">
+          <a
+            *ngIf="
+              (element.name === 'Website' || element.name === 'Link') &&
+                element.value;
+              else text
+            "
+            mat-button
+            class="Contact-link"
+            href="{{ element.value }}"
+            target="_blank"
+          >
+            {{ element.value }}
+          </a>
+          <ng-template #text>
+            <span>{{ element.value }}</span>
+          </ng-template>
+        </td>
       </ng-container>
 
       <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
@@ -60,27 +85,27 @@ export class StockContactComponent implements OnChanges {
   private prepareData() {
     const data = this.contact;
     const tableData = [
-      { name: "Exec Name", value: data["Exec Name"] },
-      { name: "Email 2", value: data["Email 2"] },
-      { name: "Email 1", value: data["Email 1"] },
-      { name: "Market Cap", value: data["Mkt Cap"] },
+      { name: "Name", value: data.Name },
       { name: "Website", value: data.Website },
+      { name: "Telephone", value: data.Telephone },
+      { name: "Address", value: data.Address },
+      { name: "Exec Name", value: data["Exec Name"] },
+      { name: "Exec Title", value: data.ExecTitle },
+      { name: "Market Cap", value: data["Mkt Cap"] },
+      { name: "Industry", value: data.Industry },
+      { name: "Sector", value: data.Sector },
       { name: "Description", value: data.Description },
+      { name: "Exec Salutation", value: data["Exec Salutation"] },
+      { name: "Email 1", value: data["Email 1"] },
+      { name: "Email 2", value: data["Email 2"] },
+      { name: "Exec First Name", value: data["Exec First Name"] },
+      { name: "Exec Middle Name", value: data["Exec Middle Name"] },
+      { name: "Exec Last Name", value: data["Exec Last Name"] },
+      { name: "Exec Pay", value: data["Exec Pay"] },
       { name: "Exchange", value: data.Exchange },
       { name: "Symbol", value: data.Symbol },
       { name: "Source", value: data.source },
       { name: "Link", value: data.Link },
-      { name: "Address", value: data.Address },
-      { name: "Exec First Name", value: data["Exec First Name"] },
-      { name: "Exec Middle Name", value: data["Exec Middle Name"] },
-      { name: "Exec Last Name", value: data["Exec Last Name"] },
-      { name: "Name", value: data.Name },
-      { name: "Industry", value: data.Industry },
-      { name: "Sector", value: data.Sector },
-      { name: "Exec Title", value: data.ExecTitle },
-      { name: "Exec Salutation", value: data["Exec Salutation"] },
-      { name: "Telephone", value: data.Telephone },
-      { name: "Exec Pay", value: data["Exec Pay"] },
       { name: "Cap Type", value: data.cap_type }
     ];
 
